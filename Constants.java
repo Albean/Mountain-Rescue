28de3c84550d291c60063dbe1d1c69c7ebc1d2b4
Albean <alan.reidy22@gmail.com>
2015-01-19 14:21:57 +0000

    Create Constants.java

---
+++ Constants.java
@@ -0,0 +1,14 @@
+package com.fyp.mountainRescue2;
+
+public class Constants{
+	public static final String CONSUMER_KEY ="VhwE2GccgRS0R29EZCb8FA";
+	public static final String CONSUMER_SECRET = "6HKLL34it2vmziCNdP49927DuH8vEoieiV9KdhcM";
+	
+	public static final String REQUEST_URL = "https://api.twitter.com/oauth/request_token";
+	public static final String ACCESS_URL = "https://api.twitter.com/oauth/access_token";
+	public static final String AUTHORIZE_URL = "https://api.twitter.com/oauth/authorize";
+	
+	final public static String CALLBACK_SCHEME ="http://otweet.com/authenticated";
+	final public static String CALLBACK_URL = CALLBACK_SCHEME + "://callback";
+	
+}
